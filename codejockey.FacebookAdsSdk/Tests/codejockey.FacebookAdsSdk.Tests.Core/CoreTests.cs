c7d9cd02f3b2e0cab077e49d4ada57ccda6a7e09
Eric Brown <ericsb@live.com>
2014-12-16 02:43:43 -0500

    Code commit, work begins.

---
+++ codejockey.FacebookAdsSdk/Tests/codejockey.FacebookAdsSdk.Tests.Core/CoreTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace codejockey.FacebookAdsSdk.Tests.Core
+{
+    [TestClass]
+    public class CoreTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
